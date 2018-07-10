21330cb3eea8ad337b060e923b0243bb6823e5e2
Ryan Hsueh <freestyle0103@gmail.com>
2018-07-10 10:38:21 +0800

    Add NavigationView in DrawerLayout

--- app/src/main/java/com/example/ryanhsueh/materialdesignplayground/MainActivity.java
+++ app/src/main/java/com/example/ryanhsueh/materialdesignplayground/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.ryanhsueh.materialdesignplayground;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -22,12 +24,23 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
 
-        mDrawer = findViewById(R.id.drawer_layout);
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
 //            actionBar.setHomeAsUpIndicator(android.R.drawable.ic_menu_add);
         }
+
+        mDrawer = findViewById(R.id.drawer_layout);
+        NavigationView nav = findViewById(R.id.nav_view);
+        nav.setCheckedItem(R.id.nav_dashboard);
+        nav.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                mDrawer.closeDrawers();
+                return true;
+            }
+        });
+
     }
 
     @Override
