3654db3a840254fa807b472a080356f06dfd4e58
Ryan Hsueh <freestyle0103@gmail.com>
2018-07-10 10:05:06 +0800

    Add drawer layout

--- app/src/main/java/com/example/ryanhsueh/materialdesignplayground/MainActivity.java
+++ app/src/main/java/com/example/ryanhsueh/materialdesignplayground/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.ryanhsueh.materialdesignplayground;
 
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -9,6 +12,8 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout mDrawer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +21,13 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
+
+        mDrawer = findViewById(R.id.drawer_layout);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+//            actionBar.setHomeAsUpIndicator(android.R.drawable.ic_menu_add);
+        }
     }
 
     @Override
@@ -27,6 +39,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawer.openDrawer(GravityCompat.START);
+                break;
             case R.id.menu_call:
                 Toast.makeText(this, "Menu Call", Toast.LENGTH_SHORT).show();
                 break;
